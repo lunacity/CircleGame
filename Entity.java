9e92502a9df3c3bfc50e35dd15fa3eaaff75a659
Sean Dugan <lunacity360@gmail.com>
2014-08-18 10:36:17 -0500

    Update
    Movable sprite

--- Entity.java
+++ Entity.java
@@ -1,13 +1,95 @@
 //Entity Class
 
-package circlegame;
+package CircleGame;
 
 
 public class Entity {
+	
 	private int x = 200;
 	private int y = 200;
-	private int xs = 5;
-	private int ys = 5;
+	private int speedX = 0;
+	private int speedY = 0;
+	
 	private boolean attacked = false;
 	
+	private boolean movingLeft = false;
+	private boolean movingRight = false;
+	private boolean movingUp = false;
+	private boolean movingDown = false;
+	
+	
+	public void update() {
+		x += speedX;
+		y += speedY;
+	}
+	
+	public void moveRight() {
+		speedX=5;
+	}
+		
+	public void moveLeft() {
+		speedX=-5;
+	}
+		
+	public void moveUp() {
+		speedY=-5;
+	}
+		
+	public void moveDown() {
+		speedY=5;
+	}
+	
+	public void stop() {
+		speedX = 0;
+		speedY = 0;
+	}
+		
+	public void attack() {
+		if(attacked==false){
+			//attackanimationhere;
+			attacked=true;
+		}
+	}
+
+	public void setCenterY(int centerY) {
+		this.y = centerY;
+	}
+
+	public void setCenterX(int centerX) {
+		this.x = centerX;
+	}
+	
+	public int getCenterX(){
+		return x;
+	}
+	
+	public int getCenterY() {
+		return y;
+	}
+
+	public int setSpeedY(int speedX) {
+		this.speedX = speedX;
+		return x;
+	}
+
+	public int setSpeedX(int speedY) {
+		this.speedY = speedY;
+		return y;
+	}
+	
+	public int getSpeedX(){
+		return speedX;
+	}
+	
+	public int getSpeedY() {
+		return speedY;
+	}
+	
+	public boolean isAttack() {
+		return attacked;
+	}
+	
+	public void setAttack(boolean attacked) {
+		this.attacked = attacked;
+	}
 }
--- Main.java
+++ Main.java
@@ -1,28 +1,50 @@
 //Entity Class
 
-package circlegame;
+package CircleGame;
 
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.net.URL;
 
 
 public class Main extends Applet implements Runnable, KeyListener {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Image image, character;
+	private Entity player;
+	private Graphics second;
+	private URL base;
 
 	@Override
 	public void init() {
 		setSize(800,600);
-		setBackground(Color.BLACK);
+		setBackground(Color.WHITE);
 		setFocusable(true);
 		addKeyListener(this);
 		Frame frame = (Frame) this.getParent().getParent();
         frame.setTitle("CircleGame Alpha 1.0");
+        try {
+			base = getDocumentBase();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+        character = getImage(base, "data/circle.png");
+        
 	}
 
 	@Override
 	public void start() {
+		
+		player = new Entity();
+		
 		Thread thread = new Thread(this);
 		thread.start();
 	}
@@ -37,14 +59,11 @@ public class Main extends Applet implements Runnable, KeyListener {
 
 	}
 
-	public static void main(String[] args) {
-
-	}
 
 	@Override
 	public void run() {
 		while (true) {
-			//update();
+			player.update();
 	        repaint();
 	        try {
 	            Thread.sleep(17);
@@ -53,32 +72,56 @@ public class Main extends Applet implements Runnable, KeyListener {
 	        }
 	    }
 	}
+	
+	public void update(Graphics g) {
+		if(image==null) {
+			image = createImage(this.getWidth(), this.getHeight());
+			second = image.getGraphics();
+		}
+		
+		second.setColor(getBackground());
+		second.fillRect(0, 0, getWidth(), getHeight());
+		second.setColor(getForeground());
+		paint(second);
+
+		g.drawImage(image, 0, 0, this);
+	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	@Override
+	public void paint(Graphics g) {
+		g.drawImage(character, player.getCenterX(), player.getCenterY(), this);
+
+	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		System.out.println("Pressed " +e.getKeyCode());
-		
 		switch(e.getKeyCode()) {
 		case KeyEvent.VK_W:
 			System.out.println("Move up");
+			player.moveUp();
 			break;
 		case KeyEvent.VK_S:
 			System.out.println("Move down");
+			player.moveDown();
 			break;
 		case KeyEvent.VK_A:
 			System.out.println("Move left");
+			player.moveLeft();
 			break;
 		case KeyEvent.VK_D:
 			System.out.println("Move right");
+			player.moveRight();
 			break;
 		case KeyEvent.VK_SPACE:
 			System.out.println("Attack!");
+			player.attack();
 			break;
 		}
 	}
@@ -89,15 +132,19 @@ public class Main extends Applet implements Runnable, KeyListener {
 		switch(e.getKeyCode()) {
 		case KeyEvent.VK_W:
 			System.out.println("Stop move up");
+			player.stop();
 			break;
 		case KeyEvent.VK_S:
 			System.out.println("Stop move down");
+			player.stop();
 			break;
 		case KeyEvent.VK_A:
 			System.out.println("Stop move left");
+			player.stop();
 			break;
 		case KeyEvent.VK_D:
 			System.out.println("Stop move right");
+			player.stop();
 			break;
 		case KeyEvent.VK_SPACE:
 			System.out.println("Stop Attack!");
